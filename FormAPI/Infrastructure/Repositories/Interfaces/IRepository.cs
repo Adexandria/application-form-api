2ef800b4f21e4c110e737fe1cbfd94557d3ad4d5
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2024-06-19 20:58:34 +0100

    Add generic type to handle container

---
+++ FormAPI/Infrastructure/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using FormAPI.Models;
+
+namespace FormAPI.Infrastructure.Repositories.Interfaces
+{
+    /// <summary>
+    /// A generic class to manage model container
+    /// </summary>
+    /// <typeparam name="T">Base class type</typeparam>
+    public interface IRepository<T> where T : BaseClass
+    {
+        /// <summary>
+        /// Adds new item to the container
+        /// </summary>
+        /// <param name="item">Manages model</param>
+        /// <returns>A boolean value to denote if it was successfully</returns>
+        Task<bool> AddItem(T item);
+    }
+}
