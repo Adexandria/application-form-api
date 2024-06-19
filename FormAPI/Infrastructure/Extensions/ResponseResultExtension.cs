9f2cbe98a11895d35e22a4fd0b31284d9a54eb77
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2024-06-19 20:53:22 +0100

    Add extension to map response to action result

---
+++ FormAPI/Infrastructure/Extensions/ResponseResultExtension.cs
@@ -0,0 +1,45 @@
+﻿using FormAPI.Infrastructure.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormAPI.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Map Custom response result to mvc action result
+    /// </summary>
+    public static class ResponseResultExtension
+    {
+        /// <summary>
+        /// Maps response to mvc
+        /// </summary>
+        /// <param name="response">Handles response result</param>
+        /// <returns>Action result</returns>
+        public static ActionResult Response(this ResponseResult response)
+        {
+            return response.StatusCode switch 
+            {
+                StatusCodes.Status200OK => new OkResult(),
+                StatusCodes.Status201Created=> new CreatedResult(),
+                StatusCodes.Status404NotFound => new NotFoundObjectResult(response.Errors),
+                _ => new BadRequestObjectResult(response.Errors)
+            };
+        }
+
+
+        /// <summary>
+        /// Maps response to mvc
+        /// </summary>
+        /// <typeparam name="T">Any type</typeparam>
+        /// <param name="response">Handles response result</param>
+        /// <returns>Action result</returns>
+        public static ActionResult Response<T>(this ResponseResult<T> response)
+        {
+            return response.StatusCode switch
+            {
+                StatusCodes.Status200OK => new OkObjectResult(response.Data),
+                StatusCodes.Status201Created => new CreatedResult(),
+                StatusCodes.Status404NotFound => new NotFoundObjectResult(response.Errors),
+                _ => new BadRequestObjectResult(response.Errors)
+            };
+        }
+    }
+}
