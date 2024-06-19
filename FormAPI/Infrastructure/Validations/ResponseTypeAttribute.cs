b70e18af045930cea5c835b9d365d1ed64070dcf
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2024-06-19 21:00:30 +0100

    Add validator to allow specific data types

---
+++ FormAPI/Infrastructure/Validations/ResponseTypeAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace FormAPI.Infrastructure.Validations
+{
+    /// <summary>
+    /// Validates response type for form response
+    /// </summary>
+    /// <param name="errorMessage"></param>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ResponseTypeAttribute(string errorMessage): ValidationAttribute
+    {
+
+        /// <summary>
+        /// Validates value
+        /// </summary>
+        /// <param name="value">Payload value</param>
+        /// <param name="validationContext">Handles validation context</param>
+        /// <returns>Boolean value to prove it was accepted</returns>
+        /// 
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not JsonElement node)
+            {
+                return new ValidationResult(errorMessage);
+            };
+
+
+            return node switch
+            {
+                JsonElement json when json.ValueKind == JsonValueKind.False || json.ValueKind == JsonValueKind.True => ValidationResult.Success,
+                JsonElement json when json.ValueKind == JsonValueKind.Array => ValidationResult.Success,
+                JsonElement json when json.ValueKind == JsonValueKind.String => ValidationResult.Success,
+                JsonElement json when json.ValueKind == JsonValueKind.Number => ValidationResult.Success,
+                _ => new ValidationResult(errorMessage)
+            };
+        }
+    }
+}
