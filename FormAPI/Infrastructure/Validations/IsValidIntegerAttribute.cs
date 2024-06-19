828b05a2529075f3758085f225b24123aa783ca9
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2024-06-19 20:59:55 +0100

    Add validator to validate integer

---
+++ FormAPI/Infrastructure/Validations/IsValidIntegerAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace FormAPI.Infrastructure.Validations
+{
+    /// <summary>
+    /// Validates integer value
+    /// </summary>
+    /// <param name="errorMessage">Error message</param>
+    /// <param name="minValue">Minimum value accepted</param>
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property, AllowMultiple = false)]
+    public class IsValidIntegerAttribute(string errorMessage,int minValue = 0) : ValidationAttribute
+    {
+
+        /// <summary>
+        /// Validates value
+        /// </summary>
+        /// <param name="value">Payload value</param>
+        /// <param name="validationContext">Handles validation context</param>
+        /// <returns>Boolean value to prove it was accepted</returns>
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null )
+            {
+                return new ValidationResult(errorMessage);
+            }
+
+            return Convert.ToInt32(value) >= _minValue ? ValidationResult.Success : new ValidationResult(errorMessage);
+        }
+      
+
+        private readonly int _minValue = minValue;
+    }
+}
