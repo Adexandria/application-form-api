9b82bb65c2ca025bf8a834c00d46d303be1217a7
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2024-06-19 21:01:10 +0100

    Add model to fetch question choice at the repository layer

---
+++ FormAPI/Infrastructure/Utility/QuestionChoice.cs
@@ -0,0 +1,26 @@
+﻿using FormAPI.Models.Questions;
+
+namespace FormAPI.Infrastructure.Utility
+{
+    /// <summary>
+    /// Model used to fetch choices from container
+    /// </summary>
+    public class QuestionChoice
+    {
+        /// <summary>
+        /// List of choices
+        /// </summary>
+        public List<Choice> Choices { get; set; }
+
+        /// <summary>
+        /// Maximum number of choices
+        /// </summary>
+        public string MaxChoices { get; set; }
+
+        /// <summary>
+        /// Question id
+        /// </summary>
+
+        public string id {  get; set; }
+    }
+}
