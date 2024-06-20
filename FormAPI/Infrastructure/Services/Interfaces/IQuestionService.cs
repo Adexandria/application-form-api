73b662e18df83b4f4df6b05cef123436bcf10e58
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2024-06-20 06:00:14 +0100

    Add service to manage question operations

---
+++ FormAPI/Infrastructure/Services/Interfaces/IQuestionService.cs
@@ -0,0 +1,51 @@
+﻿using FormAPI.Infrastructure.Utility;
+using FormAPI.Infrastructure.Validations;
+using FormAPI.Models.DTOs.QuestionDTOs.CreateQuestions;
+using FormAPI.Models.DTOs.QuestionDTOs.FetchQuestions;
+using FormAPI.Models.DTOs.QuestionDTOs.UpdateQuestions;
+using FormAPI.Models.Questions;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormAPI.Infrastructure.Services.Interfaces
+{
+    /// <summary>
+    /// Manages question operations
+    /// </summary>
+    public interface IQuestionService
+    {
+        /// <summary>
+        /// Creates new question
+        /// </summary>
+        /// <param name="formId">Form Id</param>
+        /// <param name="newQuestion">A model to manage question creation</param>
+        /// <returns>Response result</returns>
+        Task<ResponseResult> CreateQuestion( string formId,
+            CreateQuestionDto newQuestion);
+
+
+        /// <summary>
+        /// Updates an existing question
+        /// </summary>
+        /// <param name="questionId">Question id</param>
+        /// <param name="formId">Form id</param>
+        /// <param name="updateQuestion">A model used to update questions</param>
+        /// <returns>Response result</returns>
+        Task<ResponseResult> UpdateQuestion(string questionId,string formId,UpdateQuestionDto updateQuestion);
+
+        /// <summary>
+        /// Fetches questions by type
+        /// </summary>
+        /// <param name="questionType">Question type</param>
+        /// <param name="formId">Form id</param>
+        /// <returns>List of question</returns>
+        Task<ResponseResult<List<Question>>> FetchQuestionsByType( string formId, string questionType);
+
+        /// <summary>
+        /// fetch choices by type
+        /// </summary>
+        /// <param name="formId">Form id</param>
+        /// <param name="questionType">Question id</param>
+        /// <returns>A list of choices</returns>
+        Task<ResponseResult<List<QuestionChoice>>> FetchQuestionChoice(string formId, string questionType);
+    }
+}
---
+++ FormAPI/Infrastructure/Services/QuestionService.cs
@@ -0,0 +1,125 @@
+﻿using FormAPI.Infrastructure.Extensions;
+using FormAPI.Infrastructure.Repositories.Interfaces;
+using FormAPI.Infrastructure.Services.Interfaces;
+using FormAPI.Infrastructure.Utility;
+using FormAPI.Models.DTOs.QuestionDTOs.CreateQuestions;
+using FormAPI.Models.DTOs.QuestionDTOs.UpdateQuestions;
+using FormAPI.Models.Enums;
+using FormAPI.Models.Questions;
+
+namespace FormAPI.Infrastructure.Services
+{
+    /// <summary>
+    /// Manages question operations
+    /// </summary>
+    /// <param name="questionRepository">Manages question container</param>
+    /// <param name="formRepository">Manages form container</param>
+    public class QuestionService(IQuestionRepository questionRepository, IFormRepository formRepository) : BaseService, IQuestionService
+    {
+        /// <summary>
+        /// Updates an existing question
+        /// </summary>
+        /// <param name="questionId">Question id</param>
+        /// <param name="formId">Form id</param>
+        /// <param name="updateQuestion">A model used to update questions</param>
+        /// <returns>Response result</returns>
+        public async Task<ResponseResult> UpdateQuestion(string questionId, string formId, UpdateQuestionDto updateQuestion)
+        {
+            var isExist = await _formRepository.IsExist(formId);
+            if(!isExist)
+            {
+                return FailedOperation("Form does not exist", 404);
+            }
+            var currentQuestion = await _questionRepository.GetQuestion(questionId, formId);
+            if (currentQuestion == null)
+            {
+                return FailedOperation("Question does not exist", 404);
+            }
+
+           var updatedQuestion =  currentQuestion.UpdateQuestionExistingBasedOnType(updateQuestion);
+
+            var response = await _questionRepository.UpdateQuestion(updatedQuestion);
+
+            if (!response)
+            {
+                return FailedOperation("Failed to update question");
+            }
+
+            return SuccessfulOperation();
+
+        }
+
+        /// <summary>
+        /// fetches questions by type
+        /// </summary>
+        /// <param name="questionType">Question type</param>
+        /// <param name="formId">Form id</param>
+        /// <returns>A list of question</returns>
+        public async Task<ResponseResult<List<Question>>> FetchQuestionsByType(string formId, string questionType)
+        {
+            var type = Enum.Parse<QuestionType>(questionType);
+            var currentQuestions = await _questionRepository.GetQuestions(formId, type);
+
+            if (!currentQuestions.Any())
+            {
+                return FailedOperation<List<Question>>("There are no questions",404);
+            }
+
+            return SuccesfulOperation(currentQuestions);
+        }
+
+        /// <summary>
+        /// fetch choices by type
+        /// </summary>
+        /// <param name="formId">Form id</param>
+        /// <param name="questionType">Question id</param>
+        /// <returns>A list of choices</returns>
+        public async Task<ResponseResult<List<QuestionChoice>>> FetchQuestionChoice(string formId, string questionType)
+        {
+            var choices = await _questionRepository.GetQuestionChoices(formId,Enum.Parse<QuestionType>(questionType));
+
+            return SuccesfulOperation(choices);
+        }
+
+
+
+        /// <summary>
+        /// Creates new question
+        /// </summary>
+        /// <param name="formId">Form Id</param>
+        /// <param name="newQuestion">A model to manage question creation</param>
+        /// <returns>Response result</returns>
+        public async Task<ResponseResult> CreateQuestion(string formId, CreateQuestionDto newQuestion)
+        {
+            var isExist = await _formRepository.IsExist(formId);
+            if (!isExist)
+            {
+                return FailedOperation("Form does not exist", 404);
+            }
+            //Convert question to it's model (model managing the question type)
+            var question = newQuestion.CreateQuestionBasedOnType(formId);
+
+            //Persists data to the container
+            var questionResponse = await _questionRepository.AddItem(question);
+
+            if (!questionResponse)
+            {
+                return FailedOperation("Failed to add question");
+            }
+
+            return SuccessfulOperation(201);
+            
+        }
+
+        /// <summary>
+        /// Manages question container
+        /// </summary>
+        private readonly IQuestionRepository _questionRepository = questionRepository;
+
+        /// <summary>
+        /// Manages form container
+        /// </summary>
+
+        private readonly IFormRepository _formRepository = formRepository;
+    }
+}
